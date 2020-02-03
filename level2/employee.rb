fb651592490e7b5ad47772ee6359d1fd02f7236c
Silvia Patono <silvia.patono@gmail.com>
2020-02-03 18:34:05 +0100

    solution level2

--- level2/employee.rb
+++ level2/employee.rb
@@ -1,1 +1,24 @@
-employee.rb
+# Model of employee
+
+class Employee
+  attr_reader :office, :first_name, :last_name
+  attr_accessor :id
+
+  def initialize(attributes = {})
+    @id = attributes[:id]
+    @office = attributes[:office]
+    @name = attributes[:name]
+    @first_name = create_first_name(@name)
+    @last_name = create_last_name(@name)
+  end
+
+  # Create last name of employee from last name
+  def create_last_name(name)
+    name.split[0]
+  end
+
+  # Create first name of employee from full name
+  def create_first_name(name)
+    name.split[1]
+  end
+end
